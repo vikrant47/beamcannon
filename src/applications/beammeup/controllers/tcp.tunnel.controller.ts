1dabd4d9d9eaef7d9a3699ff6fa14a09d698651a
vikrant47 <vikrant.singh1990@yahoo.in>
2021-11-06 13:59:25 +0530

    http controller added

--- src/applications/beammeup/controllers/tcp.tunnel.controller.ts
+++ src/applications/beammeup/controllers/tcp.tunnel.controller.ts
@@ -3,10 +3,16 @@ import {TunnelService} from "../services/tunnel.service";
 import {Logger} from "../../../modules/services/logging/Logger";
 import {TunnelPacket, TunnelProtocol} from "../classes/pojos/tunnel.meta";
 import {TcpInChannel} from "../servers/channels/tcp.in.channel";
+import router from "../../../routes";
 
 const logger = Logger.getLogger('beammeup.TcpTunnelController');
 
 export class TcpTunnelController {
+    async onIncomingRequest(req, res) {
+        const {tunnelAlias, path} = req.params;
+        await TunnelService.instance().tunnelHttpRequest(tunnelAlias, req, res, path);
+    }
+
     async onData(connection, data) {
         if (!connection.tunnelMeta && typeof data === 'string') {
             const packet = <TunnelPacket>JSON.parse(data);
@@ -26,6 +32,7 @@ export class TcpTunnelController {
         logger.debug('Listening tcp socket events');
         TcpServer.instance().on('tcp.socket.data', async (connection, data) => {
             await this.onData(connection, data);
-        })
+        });
+        router.all('/tunnels/:tunnelAlias/:path*', this.onIncomingRequest)
     }
 }
